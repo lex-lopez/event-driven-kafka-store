d7974f68a97f34aa6b5df1a76f708b9466c22c8c
lex-lopez <l3xlop@gmail.com>
2025-06-30 20:38:51 -0600

    Adding consumer logic to send email from email-service

---
+++ email-service/src/main/java/com/alopez/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,23 @@
+package com.alopez.emailservice.kafka;
+
+import com.alopez.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consumeOrder(OrderEvent orderEvent){
+        LOGGER.info("Received message='{}'", orderEvent);
+
+        // send email to a customer
+    }
+}
